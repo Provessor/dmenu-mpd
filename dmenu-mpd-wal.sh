4cf4bd6bb11022b6ab3322d97012a8f280fdbc10
Provessor <jaiflack@gmail.com>
2018-05-13 14:25:20 +0000

    fixing dmenu variable

--- dmenu-mpd-wal.sh
+++ dmenu-mpd-wal.sh
@@ -7,27 +7,27 @@
 dmenu='dmenu -i -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15" -fn "System San Francisco Display-13"'
 
 crossfade() {
-  local crossfade=`echo -e "0\n5" | dmenu -p "Crossfade"`
+  local crossfade=`echo -e "0\n5" | $dmenu -p "Crossfade"`
   mpc crossfade $crossfade
 }
 
 current() {
   # For a formatted current output
-  #echo -e `mpc current -f "%track% - %title% - %artist% - %album%"` | dmenu -p "Crossfade"
+  #echo -e `mpc current -f "%track% - %title% - %artist% - %album%"` | $dmenu -p "Crossfade"
 
-  echo -e `mpc current` | dmenu -p "Crossfade"
+  echo -e `mpc current` | $dmenu -p "Crossfade"
 }
 
 seek() {
-  local seek=`echo -e "[+-]\n[<HH:MM:SS>]\nor\n<[+-]0-100>%>" | dmenu -p "Seek"`
+  local seek=`echo -e "[+-]\n[<HH:MM:SS>]\nor\n<[+-]0-100>%>" | $dmenu -p "Seek"`
   mpc seek "$seek"
 }
 
 search() {
-  local artist=`mpc list albumartist | dmenu -p "Artist"`
+  local artist=`mpc list albumartist | $dmenu -p "Artist"`
   local albumlist=`mpc list album artist "$artist"`
 
-  local album=`echo -e "[ALL]\n$albumlist" | dmenu -p "Album"`
+  local album=`echo -e "[ALL]\n$albumlist" | $dmenu -p "Album"`
 
   mpc clear
   if [[ $album == '[ALL]' ]];
@@ -41,19 +41,19 @@ search() {
 }
 
 playlist() {
-  local track=`mpc playlist -f "%position% - %title% - %artist% - %album%" | dmenu -l 5 -p "Track"`
+  local track=`mpc playlist -f "%position% - %title% - %artist% - %album%" | $dmenu -l 5 -p "Track"`
   mpc play "${track%% *}"
 }
 
 load() {
-  local load=`mpc lsplaylists | dmenu -l 5 -p "Track"`
+  local load=`mpc lsplaylists | $dmenu -l 5 -p "Track"`
   mpc clear
   mpc load "$load"
   mpc play
 }
 
 # Get user action
-RESULT=`echo -e "Consume\nCrossfade\nCurrent\nNext\nPause\nPlay\nPrev\nRandom\nRepeat\nSingle\nSeek\nStop\nToggle\nClear\nSearch\nPlaylist\nLoad" | dmenu -p "Music"`
+RESULT=`echo -e "Consume\nCrossfade\nCurrent\nNext\nPause\nPlay\nPrev\nRandom\nRepeat\nSingle\nSeek\nStop\nToggle\nClear\nSearch\nPlaylist\nLoad" | $dmenu -p "Music"`
 
 case "$RESULT" in
   Consume) `mpc consume` ;;
